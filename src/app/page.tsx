1033d4c2440bfaabe7e218f1f1893ddbea50b599
wenfeng <florentxingwenfeng@gmail.com>
2025-08-10 19:41:07 -0400

    add dark mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,15 @@ export default function Home() {
   return (
     <div className="font-sans min-h-screen">
       <Header />
-      <main className="flex flex-col gap-[32px] w-full max-w-6xl items-center m-auto">
-        <section id="home" className="relative min-h-screen pt-12"><AboutMe/></section>
-        <section id="blog" className="relative min-h-screen pt-12"><Blog/></section>
-        <section id="projects" className="relative min-h-screen pt-12"><Projects/></section>
-        <section id="cv" className="relative min-h-screen pt-12"><CV/></section>
-        <section id="contact" className="relative min-h-screen pt-12">Contact</section>
+      <main className="border-b-2">
+        <div className="flex flex-col gap-[32px] w-full max-w-6xl items-center m-auto">
+        <section id="home" className="relative min-h-screen pt-20"><AboutMe/></section>
+        <section id="blog" className="relative min-h-screen pt-20"><Blog/></section>
+        <section id="projects" className="relative min-h-screen pt-20"><Projects/></section>
+        <section id="cv" className="relative min-h-screen pt-20"><CV/></section>
+        <section id="contact" className="relative min-h-screen pt-20">Contact</section>
+        </div>
+
       </main>
       <Footer />
     </div>
--- src/components/app/footer.tsx
+++ src/components/app/footer.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Footer() {
   return (
-    <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+    <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center pt-4 pb-4">
       <a
         className="flex items-center gap-2 hover:underline hover:underline-offset-4"
         href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
