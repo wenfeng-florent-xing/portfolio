7076b4a6d407d7236fd8e65e4fb3c9ded6158aaf
wenfeng <florentxingwenfeng@gmail.com>
2025-08-10 20:12:15 -0400

    add cards for about me

--- src/components/app/main/about-me.tsx
+++ src/components/app/main/about-me.tsx
@@ -1,8 +1,45 @@
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
 export default function AboutMe() {
   return (
-    <>
-      <h4>Wenfeng Xing</h4>
-      <p>Full-Stack Developer</p>
-    </>
+    <div className="flex flex-col items-center justify-center gap-8">
+      <h2 className="text-2xl">Wenfeng Xing</h2>
+      <div className="flex items-center justify-center gap-4 min-h-fit">
+        <Card className="w-96">
+          <CardHeader>
+            <CardTitle>Card Title</CardTitle>
+            <CardDescription>Card Description</CardDescription>
+            <CardAction>Card Action</CardAction>
+          </CardHeader>
+          <CardContent>
+            <p>Card Content</p>
+          </CardContent>
+          <CardFooter>
+            <p>Card Footer</p>
+          </CardFooter>
+        </Card>
+        <Card className="w-96">
+          <CardHeader>
+            <CardTitle>Card Title</CardTitle>
+            <CardDescription>Card Description</CardDescription>
+            <CardAction>Card Action</CardAction>
+          </CardHeader>
+          <CardContent>
+            <p>Card Content</p>
+          </CardContent>
+          <CardFooter>
+            <p>Card Footer</p>
+          </CardFooter>
+        </Card>
+      </div>
+    </div>
   );
 }
